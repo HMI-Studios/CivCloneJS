d1e26e6dfae69d1f7061f6723690772aa0b1077e
Heine574 <johannes@niemela.se>
2022-04-30 15:01:55 -0500

    added utils file

---
+++ src/server/utils.ts
@@ -0,0 +1,30 @@
+import { Coords } from './world';
+
+export const mod = (a: number, b: number): number => {
+  if (a >= 0) {
+    return a % b;
+  } else {
+    return ((a % b) + b) % b;
+  }
+}
+
+export const getAdjacentCoords = ({x, y}: Coords): Coords[] => {
+  const coordArray = [];
+  if (mod(x, 2) === 1) {
+    coordArray.push({ x: x,   y: y+1 });
+    coordArray.push({ x: x+1, y: y+1 });
+    coordArray.push({ x: x+1, y: y   });
+    coordArray.push({ x: x,   y: y-1 });
+    coordArray.push({ x: x-1, y: y   });
+    coordArray.push({ x: x-1, y: y+1 });
+  } else {
+    coordArray.push({ x: x+1, y: y   });
+    coordArray.push({ x: x+1, y: y-1 });
+    coordArray.push({ x: x,   y: y-1 });
+    coordArray.push({ x: x,   y: y+1 });
+    coordArray.push({ x: x-1, y: y-1 });
+    coordArray.push({ x: x-1, y: y   });
+  }
+
+  return coordArray;
+}
