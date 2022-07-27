bfa411f1e6114175071c2230ed810e320ce6a31b
JoelNiemela <joel@niemela.se>
2022-07-27 12:35:19 -0500

    Implement unitPositionUpdate

--- src/server/methods.ts
+++ src/server/methods.ts
@@ -22,7 +22,7 @@ export const games: { [gameID: number] : Game } = {
   0: new Game(
     // new Map(38, 38, JSON.parse(fs.readFileSync( path.join(__dirname, 'saves/0.json') ).toString()).map),
     // new Map(38, 38, ...new WorldGenerator(3634, 38, 38).generate(0.5, 0.9, 1)),
-    new Map(200, 200, ...new PerlinWorldGenerator(3634, 200, 200).generate()),
+    new Map(200, 200, ...new PerlinWorldGenerator(/*3634*/1, 200, 200).generate()),
     {
       playerCount: 1,
     }
@@ -271,6 +271,7 @@ const methods: {
       const map = world.map;
 
       let src = map.getTile(srcCoords);
+      let finalCoords = srcCoords;
 
       for (const dstCoords of path) {
         const dst = map.getTile(dstCoords);
@@ -302,6 +303,7 @@ const methods: {
         }
 
         src = dst;
+        finalCoords = dstCoords;
       }
 
       if (attack) {
@@ -316,6 +318,12 @@ const methods: {
       }
 
       game.sendUpdates();
+
+      game.sendToCiv(civID, {
+        error: [
+          ['unitPositionUpdate', [srcCoords, finalCoords]],
+        ],
+      });
     }
   },
 
