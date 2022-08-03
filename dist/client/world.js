9b418c128464e9924188136c49028ac1c43dae4e
kjniemela <johannes@niemela.se>
2022-08-03 14:16:07 -0500

    changed highlighted tiles from selectors to borders

--- dist/client/world.js
+++ dist/client/world.js
@@ -33,7 +33,7 @@ class World {
     getTile(x, y) {
         return this.tiles[this.pos(x, y)] || null;
     }
-    getNeighbors(x, y) {
+    getNeighbors(x, y, filter = true) {
         let tiles;
         if (mod(x, 2) === 1) {
             tiles = [
@@ -55,7 +55,11 @@ class World {
                 [x - 1, y],
             ];
         }
-        return tiles.filter(([x, y]) => !!this.getTile(x, y));
+        return filter ? tiles.filter(([x, y]) => !!this.getTile(x, y)) : tiles;
+    }
+    isAdjacent(posA, posB) {
+        // TODO - possibly optimize this? memoize?
+        return this.getNeighbors(posB.x, posB.y).map(coord => this.pos(...coord)).includes(this.pos(posA.x, posA.y));
     }
     // mode: 0 = land unit, 1 = sea unit; -1 = air unit
     getTilesInRange(srcX, srcY, range, mode = 0) {
--- src/client/world.ts
+++ src/client/world.ts
@@ -90,7 +90,7 @@ class World {
     return this.tiles[this.pos(x, y)] || null;
   }
 
-  getNeighbors(x: number, y: number): [number, number][] {
+  getNeighbors(x: number, y: number, filter = true): [number, number][] {
     let tiles: [number, number][];
 
     if (mod(x, 2) === 1) {
@@ -113,7 +113,12 @@ class World {
       ];
     }
 
-    return tiles.filter(([x, y]) => !!this.getTile(x, y));
+    return filter ? tiles.filter(([x, y]) => !!this.getTile(x, y)) : tiles;
+  }
+
+  isAdjacent(posA: Coords, posB: Coords): boolean {
+    // TODO - possibly optimize this? memoize?
+    return this.getNeighbors(posB.x, posB.y).map(coord => this.pos(...coord)).includes(this.pos(posA.x, posA.y));
   }
 
   // mode: 0 = land unit, 1 = sea unit; -1 = air unit
