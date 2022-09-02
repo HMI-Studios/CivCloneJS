aa96ef3abb957e372a894b83839ab9ce4530ca15
JoelNiemela <joel@niemela.se>
2022-09-02 17:31:42 -0500

    Add canSettle and canBuild checks on backend

--- src/server/game/map/index.ts
+++ src/server/game/map/index.ts
@@ -202,9 +202,20 @@ export class Map {
     }
   }
 
+  canSettleOn(tile: Tile): boolean {
+    return (
+      !tile.owner &&
+      tile.type !== 'ocean' &&
+      tile.type !== 'frozen_ocean' &&
+      tile.type !== 'mountain' &&
+      tile.type !== 'coastal' &&
+      tile.type !== 'frozen_coastal'
+    );
+  }
+
   settleCityAt(coords: Coords, name: string, civID: number): boolean {
     const tile = this.getTile(coords);
-    if (tile.owner) return false;
+    if (!this.canSettleOn(tile)) return false;
 
     const city: City = new City(coords, name, civID);
     this.cities.push(city);
@@ -229,9 +240,18 @@ export class Map {
     this.tileUpdate(coords);
   }
 
+  canBuildOn(tile: Tile, ownerID: number): boolean {
+    return (
+      tile.owner?.civID === ownerID &&
+      tile.type !== 'ocean' &&
+      tile.type !== 'frozen_ocean' &&
+      tile.type !== 'mountain'
+    );
+  }
+
   buildImprovementAt(coords: Coords, type: string, ownerID: number): void {
     const tile = this.getTile(coords);
-    if (tile.owner?.civID !== ownerID) return;
+    if (!this.canBuildOn(tile, ownerID)) return;
 
     tile.improvement = new Improvement(type);
 
